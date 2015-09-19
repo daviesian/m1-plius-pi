e0ae2ebee8450284538e560b7b8e26b325749567
Ian Davies <github@iandavies.org>
2015-09-20 00:01:19 +0100

    Update URL

--- play.sh
+++ play.sh
@@ -1,1 +1,1 @@
-sox -t mp3 http://84.46.147.81/m1plius/mp3 -t wav - | sudo /home/pi/PiFmRds/src/pi_fm_rds -audio - -freq 104.0 -ps 'Vaiva FM' -rt 'M-1 Plius. Yes, really.' &
+sox -t mp3 http://radio.m-1.fm/m1plius/mp3 -t wav - | sudo /home/pi/PiFmRds/src/pi_fm_rds -audio - -freq 104.0 -ps 'Vaiva FM' -rt 'M-1 Plius. Yes, really.' &
